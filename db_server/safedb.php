a1989280a11ac1011b223aece198e0b2fd6e41e1
eV <electron-volt@users.noreply.github.com>
2017-06-30 12:51:24 +0300

    Fix SQL-i vuln with prepared statements
    NB
    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
    should also be used

--- db_server/safedb.php
+++ db_server/safedb.php
@@ -1,17 +1,21 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 $location ="sqlite:".__DIR__."/safebrowsing.sqllite3";
 
 if (isset($_GET['finger']))
 {
+    $finger = $_GET['finger'];
     $dbh  = new PDO($location) or die("cannot open the database");
-
-    $query =  "SELECT * FROM browsertab WHERE fingerprint='".$_GET['finger']."'";
+    # $query =  "SELECT * FROM browsertab WHERE fingerprint='".$_GET['finger']."'";
+    $stmt = $dbh->prepare("SELECT * FROM browsertab WHERE fingerprint=?");
+    $stmt->execute($finger);
+    $result = $stmt->fetch(); 
     $count = 0;
-    $res = $dbh->query($query);
-    if($res) {
-        foreach ($res as $row) {
-            $name['name'] = $row[1];
+    # $res = $dbh->query($query);
+    if($result) {
+        foreach ($result as $row) {
+            $name["name"] = $row[1];
             $name["status"]=0;
             $count = $count + 1;
             echo json_encode($name);
@@ -19,6 +23,8 @@ if (isset($_GET['finger']))
             die();
         }
     }
+    $stmt->close();
+    
     if ($count==0)
     {
         if (isset($_GET['check']))
@@ -27,17 +33,21 @@ if (isset($_GET['finger']))
             echo json_encode($arr);
             die();
         }
-        $query =  "INSERT INTO browsertab VALUES('".$_GET['finger']."','".$_GET['name']."')";
-        $dbh->query($query);
-        $res = $dbh->query($query);
+        $username = $_GET['name'];
+        # $query =  "INSERT INTO browsertab VALUES('".$_GET['finger']."','".$_GET['name']."')";
+        #$dbh->query($query);
+        #$res = $dbh->query($query);
+        $query = "INSERT INTO browsertab VALUES (?,?)"
+        $dbh->prepare($query)->execute([$finger, $username]);
+        
         $ar["status"]=1;
         echo json_encode($ar);
-
     }
 }
-
 else
 {
     echo "Not a website!";
 }
 ?>
+
+

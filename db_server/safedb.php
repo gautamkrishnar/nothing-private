43b05c8c84e5895c8270714141cbe380206faff6
Sjöblom <eveliina.sjoblom@cgi.com>
2017-07-10 22:21:18 +0300

    prepared statements for PDO in safedb.php, some minor typos fixed

--- db_server/safedb.php
+++ db_server/safedb.php
@@ -1,22 +1,22 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-$location ="sqlite:".__DIR__."/safebrowsing.sqllite3";
+$location = "sqlite:".__DIR__."/safebrowsing.sqllite3";
 
 if (isset($_GET['finger']))
 {
-    $finger = $_GET['finger'];
-    $dbh  = new PDO($location) or die("cannot open the database");
-    # $query =  "SELECT * FROM browsertab WHERE fingerprint='".$_GET['finger']."'";
+	
+    $dbh = new PDO($location) or die("cannot open the database"); 
+
     $stmt = $dbh->prepare("SELECT * FROM browsertab WHERE fingerprint=?");
-    $stmt->execute($finger);
+    $stmt->execute([$_GET['finger']]);
     $result = $stmt->fetch(); 
+	
     $count = 0;
-    # $res = $dbh->query($query);
-    if($result) {
+    if ($result) {
         foreach ($result as $row) {
             $name["name"] = $row[1];
-            $name["status"]=0;
+            $name["status"] = 0;
             $count = $count + 1;
             echo json_encode($name);
             $dbh = null;
@@ -25,22 +25,19 @@ if (isset($_GET['finger']))
     }
     $stmt->close();
     
-    if ($count==0)
+    if ($count == 0)
     {
         if (isset($_GET['check']))
         {
-            $arr["status"]=3;
+            $arr["status"] = 3;
             echo json_encode($arr);
             die();
         }
-        $username = $_GET['name'];
-        # $query =  "INSERT INTO browsertab VALUES('".$_GET['finger']."','".$_GET['name']."')";
-        #$dbh->query($query);
-        #$res = $dbh->query($query);
+		
         $query = "INSERT INTO browsertab VALUES (?,?)"
-        $dbh->prepare($query)->execute([$finger, $username]);
+        $dbh->prepare($query)->execute([$_GET['finger'], $_GET['name']]);
         
-        $ar["status"]=1;
+        $ar["status"] = 1;
         echo json_encode($ar);
     }
 }

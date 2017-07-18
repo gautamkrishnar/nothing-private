affe67eaca2fc62003ee7e67e80aaa5a4f950516
brunomassa <massa_ferreira@hotmail.com>
2017-07-18 14:20:57 +0100

    added forget me button
    Added a button to remove your data from database.

--- db_server/safedb.php
+++ db_server/safedb.php
@@ -8,42 +8,42 @@ if (isset($_GET['finger']))
 	
     $dbh = new PDO($location) or die("cannot open the database"); 
 
-    $stmt = $dbh->prepare("SELECT * FROM browsertab WHERE fingerprint=?");
-    $stmt->execute([$_GET['finger']]);
+    $stmt = $dbh->prepare('SELECT * FROM browsertab WHERE fingerprint=?') or trigger_error($dbh->error, E_USER_ERROR);
+    $stmt->execute([$_GET['finger']]) or trigger_error($stmt->error, E_USER_ERROR);
     $result = $stmt->fetch(); 
 	
     $count = 0;
     if ($result) {
         foreach ($result as $row) {
-            $name["name"] = $row[1];
-            $name["status"] = 0;
+            $name['name'] = $row[1];
+            $name['status'] = 0;
             $count = $count + 1;
             echo json_encode($name);
             $dbh = null;
             die();
         }
     }
-    $stmt->close();
     
     if ($count == 0)
     {
         if (isset($_GET['check']))
         {
-            $arr["status"] = 3;
+            $arr['status'] = 3;
             echo json_encode($arr);
             die();
         }
 		
-        $query = "INSERT INTO browsertab VALUES (?,?)"
+        $query = 'INSERT INTO browsertab VALUES (?,?)';
         $dbh->prepare($query)->execute([$_GET['finger'], $_GET['name']]);
         
-        $ar["status"] = 1;
+        $ar['status'] = 1;
         echo json_encode($ar);
+		$dbh=null;
     }
 }
 else
 {
-    echo "Not a website!";
+    echo "Not&nbsp;a&nbsp;website!";
 }
 ?>
 
--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-
+/*jshint sub:true*/
 
 function savefingerprint(check)
 {
+	"use strict";
     var param = "";
     var client = new ClientJS(); // Create A New Client Object
     var fingerprint = client.getFingerprint(); // Calculate Device/Browser Fingerprint
     
-    if (check==0)
+    if (check===0)
         {
             param = encodeURI("?finger="+fingerprint+"&check=1");   
         }
     else{
             var name = document.getElementById("name").value;
             
-            if (name == undefined||name =="")
+            if (name === undefined||name ==="")
             {
                 name="NoNameEntered :D";
             }
@@ -27,17 +28,17 @@ function savefingerprint(check)
     
 
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             var arr = JSON.parse(this.responseText);
-            if(arr['status']==0)
+            if(arr['status']===0)
                 {
                     rendernewtabpage(arr['name']);
                 }
-            else if(arr['status']==1)
+            else if(arr['status']===1)
                 {
                     rendersubmit(name);
                 }
-            else if(arr['status']==3)
+            else if(arr['status']===3)
                 {
                     rendermain();
                 }
@@ -47,16 +48,46 @@ function savefingerprint(check)
     xhr.send();
 }
 
+function forgetme(){
+	
+	"use strict";
+    var param = "";
+    var client = new ClientJS(); // Create A New Client Object
+    var fingerprint = client.getFingerprint(); // Calculate Device/Browser Fingerprint
+    
+    param = encodeURI("?finger="+fingerprint);
+            
+   
+    var xhr = new XMLHttpRequest();
+    var url="http://belikebill.azurewebsites.net/areyousafe/forgetme.php";
+    
+    
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var responsetext=JSON.parse(this.responseText);
+			if(responsetext['state']===1){
+				location.reload();
+			}
+        }
+    };
+    xhr.open("GET", url+param);
+    xhr.send();
+}
+
 function rendernewtabpage(name){
+	'use strict';
     document.getElementById("maindiv").innerHTML='<h3>Welcome back to the website <b>'+name+'</b>. If I can remember your name even though you are using the <b>Private browsing</b> / <b>Incognito mode</b>, I can surely track you.<br/><br/>. This is what big companies are doing. They are spying on you even  when you are using the <b>Private browsing</b> or <b>Incognito mode</b>.<br/><br/><a href="https://github.com/gautamkrishnar/nothing-private" target="_blank">Read more</a><br/><br/>If you liked this project, don\'t forget to give it a Star:<br/></h3>';
-    document.getElementById("user").innerHTML="You are <em>"+name+"!</em>"
+    document.getElementById("user").innerHTML='You are <em>'+name+'!</em><br><br><input type="button" class="btn" onclick="forgetme()" value="Forget Me!" /><br><br>';
 }
 function rendersubmit(name)
 {
-    document.getElementById("maindiv").innerHTML='<h3><b>Thank you! '+name+'</b> Let\'s see the magic... <br/><br/>Now open a <b>Private browsing</b> or <b>Incognito window</b> on your browser and visit <b>www.nothingprivate.ml</b> to see the magic...</h3>';
+	'use strict';
+    document.getElementById("maindiv").innerHTML='<h3><b>Thank you! '+name+'</b> Let\'s see the magic... <br/><br/>Now open a <b>Private browsing</b> or <b>Incognito window</b> on your browser and visit <b>www.nothingprivate.ml</b> to see the magic...</h3><br><br><input type="button" class="btn" onclick="forgetme()" value="Forget Me!" /><br><br>';
     
 }
 function rendermain()
 {
+	'use strict';
     document.getElementById("maindiv").innerHTML='<div id="maindiv"><h3>Do you think that switching to your browser\'s <b>Private browsing mode</b> or <b>Incognito mode</b> will make you anonymous?<br/><br/> Sorry to disappoint you, <b>you are wrong!.</b> Everyone can track you. You can check it out for yourself. Just type your name below.</h3><input type="text" name="name" id="name" class="txt" placeholder="Your Name" /><br/><br/><input type="button" class="btn" onclick="savefingerprint(1)" value="See the magic!" /><br/></div>';
 }

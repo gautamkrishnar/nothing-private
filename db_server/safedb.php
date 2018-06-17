cc817bd90cca713f0180f6de243aa4ef97b99e75
Gautam Krishna R <r.gautamkrishna@gmail.com>
2018-06-17 12:37:33 +0530

    fixing sonar cube issues

--- db_server/safedb.php
+++ db_server/safedb.php
@@ -2,20 +2,21 @@
 
 header("Access-Control-Allow-Origin: *");
 $location = "sqlite:".__DIR__."/safebrowsing.sqllite3";
-
-if (isset($_GET['finger']))
+$finger = "finger";
+$status = "status";
+if (isset($_GET[$finger]))
 {
 	
-    $dbh = new PDO($location) or die("cannot open the database"); 
+    $dbh = new PDO($location) || die("cannot open the database");
 
-    $stmt = $dbh->prepare('SELECT * FROM browsertab WHERE fingerprint=?') or trigger_error($dbh->error, E_USER_ERROR);
-    $stmt->execute([$_GET['finger']]) or trigger_error($stmt->error, E_USER_ERROR);
+    $stmt = $dbh->prepare('SELECT * FROM browsertab WHERE fingerprint=?') || trigger_error($dbh->error, E_USER_ERROR);
+    $stmt->execute([$_GET[$finger]]) || trigger_error($stmt->error, E_USER_ERROR);
     $result = $stmt->fetch(); 
 	
     $count = 0;
     if ($result) {
         $name['name'] = $result['name'];
-        $name['status'] = 0;
+        $name[$status] = 0;
 	$count = 1;
         echo json_encode($name);
         $dbh = null;
@@ -26,15 +27,15 @@ if (isset($_GET['finger']))
     {
         if (isset($_GET['check']))
         {
-            $arr['status'] = 3;
+            $arr[$status] = 3;
             echo json_encode($arr);
             die();
         }
 		
         $query = 'INSERT INTO browsertab VALUES (?,?)';
-        $dbh->prepare($query)->execute([$_GET['finger'], $_GET['name']]);
+        $dbh->prepare($query)->execute([$_GET[$finger], $_GET['name']]);
         
-        $ar['status'] = 1;
+        $ar[$status] = 1;
         echo json_encode($ar);
 	$dbh=null;
 	die();

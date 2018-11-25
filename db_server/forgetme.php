5010be34714174d81b964102a698c7b212caa3d1
Gautam krishna.R <rgautamkrishna@gmail.com>
2018-11-25 14:40:09 +0530

    removed unwanted code

--- db_server/forgetme.php
+++ db_server/forgetme.php
@@ -6,19 +6,13 @@ $finger = "finger";
 
 if (isset($_GET[$finger]))
 {
-    $stmt = $mysqli->prepare('SELECT * FROM browsertab WHERE fingerprint=?');
-    $stmt->bind_param("s", $_GET[$finger]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while($row = $result->fetch_assoc()) {
-        $stmt = $mysqli->prepare('DELETE FROM browsertab WHERE fingerprint=?');
-        $stmt->bind_param("s", $_GET[$finger]);
-        $stmt->execute();
-		$forgeted['state']=1;
-        echo json_encode($forgeted);
-        $mysqli->close();
-        die();
-    }
+   $stmt = $mysqli->prepare('DELETE FROM browsertab WHERE fingerprint=?');
+   $stmt->bind_param("s", $_GET[$finger]);
+   $stmt->execute();
+   $forgeted['state']=1;
+   echo json_encode($forgeted);
+   $mysqli->close();
+   die();
 }
 else
 {

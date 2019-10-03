42ade06e7f3eb4353fb84e0472231d975648c888
quadratrund <56112624+quadratrund@users.noreply.github.com>
2019-10-03 20:00:48 +0200

    create a database truncation script (#73)
    * It truncates the existing entries in the nothing private database.
    * It increments the number saved in visitors.txt by the number of deleted rows

---
+++ db_server/cron.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/connection.php';
+
+$stmt = $mysqli->prepare('SELECT count(*) cnt FROM browsertab');
+$stmt->execute();
+$stmt->bind_result($count_from_db);
+$stmt->fetch();
+$stmt->free_result();
+
+$visitors_file_name = __DIR__ . '/visitors.txt';
+$count_from_file = (int)@file_get_contents($visitors_file_name);
+file_put_contents($visitors_file_name, $count_from_db + $count_from_file);
+
+$stmt = $mysqli->prepare('TRUNCATE TABLE browsertab');
+$stmt->execute();

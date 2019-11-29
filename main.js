71d779994f5a0bcab964c2af35e43a9ce88531fb
gautamkrishnar <r.gautamkrishna@gmail.com>
2019-11-30 00:01:01 +0530

    Added requestIdleCallback, refactoring the code for more readability.

--- main.js
+++ main.js
@@ -1,48 +1,75 @@
 /*jshint sub:true*/
 "use strict";
 
-/**
- * Function to handle saving of fingerprint and initial loading of API.
- * @param check flag specify certain tasks 0 => Initial checking of whether the fingerprint exists in db.
- */
-
 const API_ROOT = "https://nothing-private-api.gautamkrishnar.com";
+let Fingerprint = ''; // global variable to save the fingerprint
 
-function savefingerprint(check) {
-    var param = "";
-    var client = new ClientJS(); // Create A New Client Object
-    var fingerprint = client.getFingerprint(); // Calculate Device/Browser Fingerprint
+/**
+ * Function that calculates fingerprint
+ */
+function calculateFingerprint() {
+    let client = new ClientJS(); // Create A New Client Object
+    return client.getFingerprint(); // Calculate Device/Browser Fingerprint
+}
 
+/**
+ * function that calculates the fingerprint when the page finishes loading. It will save fingerprint to
+ * the global variable
+ */
+function pageLoader() {
+    // noinspection JSUnresolvedVariable
+    if (window.requestIdleCallback) {
+        // Browsers with request idle callback
+        window.requestIdleCallback(function () {
+            Fingerprint = calculateFingerprint();
+            saveFingerPrintAPICall(true);
+        });
+    } else {
+        // Fallback to native setTimeout for old browsers
+        setTimeout(function () {
+            Fingerprint = calculateFingerprint();
+            saveFingerPrintAPICall(true);
+        }, 500);
+    }
+}
+
+/**
+ * Function to do the API Call to the backend server to check fingerprint. Also handles rendering of messages.
+ * @param check - true: Initial api call
+ */
+function saveFingerPrintAPICall(check) {
+    let param = "";
+    let name = "";
     // Constructing GET Params
-    if (check === 0) {
+    if (check) {
         // Checks whether the fingerprint already exists in DB.
-        param = encodeURI("?finger=" + fingerprint + "&check=1");
+        param = encodeURI("?finger=" + Fingerprint + "&check=1");
     } else {
         // Saves the name with fingerprint to DB.
-        var name = document.getElementById("name").value;
+        name = document.getElementById("name").value;
 
         if (!name.trim()) {
             name = "NoNameEntered :D";
         }
-        param = encodeURI("?finger=" + fingerprint + "&name=" + name);
+        param = encodeURI("?finger=" + Fingerprint + "&name=" + name);
     }
 
     // Constructing request
-    var xhr = new XMLHttpRequest();
-    var url = API_ROOT + "/safedb.php";
+    const xhr = new XMLHttpRequest();
+    const url = API_ROOT + "/safedb.php";
 
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            var arr = JSON.parse(this.responseText);
+            const arr = JSON.parse(this.responseText);
             if (arr['status'] === 0) {
                 // Fingerprint already exists.
-                rendernewtabpage(arr['name']);
+                renderNewTabPage(arr['name']);
             } else if (arr['status'] === 1) {
                 // Fingerprint saved successfully
-                rendersubmit(name);
+                renderSubmit(name);
             } else if (arr['status'] === 3) {
                 // New user. No fingerprint found in the DB.
-                rendermain();
+                renderMain();
             }
         }
     };
@@ -56,23 +83,19 @@ function savefingerprint(check) {
 /**
  * Function to remove the saved fingerprint from the database.
  */
-function forgetme() {
-    var client = new ClientJS(); // Create A New Client Object
-    var fingerprint = client.getFingerprint(); // Calculate Device/Browser Fingerprint
-
-    // Param
-    var param = encodeURI("?finger=" + fingerprint);
+function forgetMe() {
+    const param = encodeURI("?finger=" + Fingerprint);
 
     // Constructing request
-    var xhr = new XMLHttpRequest();
-    var url = API_ROOT + "/forgetme.php";
+    const xhr = new XMLHttpRequest();
+    const url = API_ROOT + "/forgetme.php";
 
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            var responsetext = JSON.parse(this.responseText);
+            const responsetext = JSON.parse(this.responseText);
             if (responsetext['state'] === 1) {
                 document.getElementById("user").innerHTML = 'Are you <em>Anonymous?</em>';
-                rendermain();
+                renderMain();
             }
         }
     };
@@ -87,8 +110,8 @@ function forgetme() {
  * Renders the page when the fingerprint is found in the db with the users name.
  * @param name saved name of the user in the DB
  */
-function rendernewtabpage(name) {
-    document.getElementById("maindiv").innerHTML = '<h3>Welcome back to the website <b>' + htmlEncode(name) + '</b>. If I can remember your name even though you are using <b>private browsing</b> / <b>incognito mode</b>, I can surely track you.<br/><br/> This is what big companies are doing. They are spying on you even when you are using <b>private browsing</b> or <b>incognito mode</b>.<br/><br/><a href="https://github.com/gautamkrishnar/nothing-private" target="_blank">Read more</a><br/><br/><input type="button" class="btn" onclick="forgetme()" value="Forget Me!" /><br><br>If you liked this project, don\'t forget to give it a Star:<br/></h3>';
+function renderNewTabPage(name) {
+    document.getElementById("maindiv").innerHTML = '<h3>Welcome back to the website <b>' + htmlEncode(name) + '</b>. If I can remember your name even though you are using <b>private browsing</b> / <b>incognito mode</b>, I can surely track you.<br/><br/> This is what big companies are doing. They are spying on you even when you are using <b>private browsing</b> or <b>incognito mode</b>.<br/><br/><a href="https://github.com/gautamkrishnar/nothing-private" target="_blank">Read more</a><br/><br/><input type="button" class="btn" onclick="forgetMe()" value="Forget Me!" /><br><br>If you liked this project, don\'t forget to give it a Star:<br/></h3>';
     document.getElementById("user").innerHTML = 'You are <em>' + htmlEncode(name) + '!</em>';
 }
 
@@ -97,9 +120,9 @@ function rendernewtabpage(name) {
  * @param value Value to encode
  * @return {string} encoded value
  */
-function htmlEncode(value){
+function htmlEncode(value) {
     // Code to prevent xss attack
-    var temp_div = document.createElement("div");
+    const temp_div = document.createElement("div");
     temp_div.textContent = value;
     return temp_div.innerHTML;
 }
@@ -108,16 +131,16 @@ function htmlEncode(value){
  * Renders the page after submitting the fingerprint to the API
  * @param name
  */
-function rendersubmit(name) {
-    document.getElementById("maindiv").innerHTML = '<h3><b>Thank you, ' + htmlEncode(name) + '! </b> Let\'s see the magic... <br/><br/>Now open a <b>private browsing window</b> or <b>incognito window</b> on your browser and visit <b>www.nothingprivate.ml</b> to see the magic...</h3><br><br><input type="button" class="btn" onclick="forgetme()" value="Forget Me!" /><br><br>';
+function renderSubmit(name) {
+    document.getElementById("maindiv").innerHTML = '<h3><b>Thank you, ' + htmlEncode(name) + '! </b> Let\'s see the magic... <br/><br/>Now open a <b>private browsing window</b> or <b>incognito window</b> on your browser and visit <b>www.nothingprivate.ml</b> to see the magic...</h3><br><br><input type="button" class="btn" onclick="forgetMe()" value="Forget Me!" /><br><br>';
 
 }
 
 /**
  * Renders the page on initial loading of the page (No fingerprint is found)
  */
-function rendermain() {
-    document.getElementById("maindiv").innerHTML = '<div id="maindiv"><h3>Do you think that switching to your browser\'s <b>private browsing mode</b> or <b>incognito mode</b> will make you anonymous?<br/><br/> Sorry to disappoint you, <b>but you are wrong!.</b> Everyone can track you. You can check it out for yourself. Just type your name below.</h3><input type="text" name="name" id="name" class="txt" placeholder="Your Name" autofocus/><br/><br/><input type="button" class="btn" onclick="savefingerprint(1)" value="See the magic!" /><br/></div>';
+function renderMain() {
+    document.getElementById("maindiv").innerHTML = '<div id="maindiv"><h3>Do you think that switching to your browser\'s <b>private browsing mode</b> or <b>incognito mode</b> will make you anonymous?<br/><br/> Sorry to disappoint you, <b>but you are wrong!.</b> Everyone can track you. You can check it out for yourself. Just type your name below.</h3><input type="text" name="name" id="name" class="txt" placeholder="Your Name" autofocus/><br/><br/><input type="button" class="btn" onclick="saveFingerPrintAPICall(false)" value="See the magic!" /><br/></div>';
 }
 
 /**
@@ -125,7 +148,7 @@ function rendermain() {
  */
 function reload() {
     document.getElementById("maindiv").innerHTML = '<div id="maindiv"><h3>Loading... please wait...</h3></div>';
-    savefingerprint(0);
+    saveFingerPrintAPICall(false);
 }
 
 /**
